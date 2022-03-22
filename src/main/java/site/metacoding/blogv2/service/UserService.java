7914e4b60fb68cef10b4f97027c97e1143154966
yuseunghyun <ysh7091@gmail.com>
2022-03-22 16:51:37 +0900

    7.로그인 완료

--- src/main/java/site/metacoding/blogv2/service/UserService.java
+++ src/main/java/site/metacoding/blogv2/service/UserService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.domain.user.UserRepository;
 import site.metacoding.blogv2.web.api.dto.user.JoinDto;
+import site.metacoding.blogv2.web.api.dto.user.LoginDto;
 
 @RequiredArgsConstructor
 @Service // 컴포넌트 스캔시에 IOC 컨테이너에 등록됨.
@@ -18,5 +19,12 @@ public class UserService {
     public User 회원가입(JoinDto joinDto) {
         // save하면 db에 Insert하고 insert된 결과를 다시 return 해준다.
         return userRepository.save(joinDto.toEntity());
+    }
+
+    // 받아서 userRepositoy에 줘야함. login 처리 쿼리를 JPA에서 제공해주지 않는다. nativeQuery는 하나씩 넘겨준다.
+    // 한 번에 안 넘김
+    public User 로그인(LoginDto loginDto) {
+        User userEntity = userRepository.mLogin(loginDto.getUsername(), loginDto.getPassword());
+        return userEntity;
     }
 }
--- src/main/java/site/metacoding/blogv2/web/api/UserApiController.java
+++ src/main/java/site/metacoding/blogv2/web/api/UserApiController.java
@@ -1,18 +1,23 @@
 package site.metacoding.blogv2.web.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.service.UserService;
 import site.metacoding.blogv2.web.api.dto.ResponseDto;
 import site.metacoding.blogv2.web.api.dto.user.JoinDto;
+import site.metacoding.blogv2.web.api.dto.user.LoginDto;
 
 @RequiredArgsConstructor
 @RestController
 public class UserApiController {
     private final UserService userService;
+    private final HttpSession session;
 
     @PostMapping("/api/join")
     public ResponseDto<String> join(@RequestBody JoinDto joinDto) {
@@ -21,4 +26,15 @@ public class UserApiController {
         return new ResponseDto<String>(1, "회원가입성공", null);
     }
 
+    @PostMapping("/api/login")
+    public ResponseDto<String> login(@RequestBody LoginDto loginDto) {
+        User userEntity = userService.로그인(loginDto);
+
+        // 오류가 터지지는 않고 알려줌의 용도
+        if (userEntity == null) {
+            return new ResponseDto<String>(-1, "로그인실패", null);
+        }
+        session.setAttribute("principal", userEntity);
+        return new ResponseDto<String>(1, "로그인성공", null);
+    }
 }
