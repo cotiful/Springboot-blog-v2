7912da983b26703e993c211da3ab96596dd8c2b0
yuseunghyun <ysh7091@gmail.com>
2022-03-31 10:09:45 +0900

    오류 확인 후 처리 완료

--- src/main/java/site/metacoding/blogv2/domain/comment/Comment.java
+++ src/main/java/site/metacoding/blogv2/domain/comment/Comment.java
@@ -2,55 +2,45 @@ package site.metacoding.blogv2.domain.comment;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
-import org.hibernate.annotations.ColumnDefault;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import site.metacoding.blogv2.domain.post.Post;
 import site.metacoding.blogv2.domain.user.User;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Entity
 @EntityListeners(AuditingEntityListener.class)
-public class Comment { // N드라이빙 테이블, FK의 주인
+@Entity
+public class Comment { // Post 1 1, Comment N 1
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(length = 300, nullable = false)
-    private String title;
-
-    @Lob
-    @Column(nullable = false)
     private String content;
 
-    @ColumnDefault("0")
-    @Column(nullable = false)
-    private Integer pageCount; // 조회수
+    @JoinColumn(name = "postId")
+    @ManyToOne
+    private Post post;
 
     @JoinColumn(name = "userId")
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne
     private User user;
 
-    @CreatedDate
+    @CreatedDate // insert
     private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime updateDate;
-
 }
--- src/main/java/site/metacoding/blogv2/web/UserController.java
+++ src/main/java/site/metacoding/blogv2/web/UserController.java
@@ -1,6 +1,5 @@
 package site.metacoding.blogv2.web;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -13,29 +12,28 @@ import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.service.UserService;
 
 @RequiredArgsConstructor
-@Controller // 파일
+@Controller
 public class UserController {
     private final UserService userService;
     private final HttpSession session;
 
-    // 앱은 이 친구 요청할까? 아니
+    // 앱은 이 친구 요청 할까? 아니!!
     // 웹은 이 친구 요청해야함.
     @GetMapping("/s/user/{id}")
-    public String userInfo(@PathVariable Integer id /* , Model model */) {
-        // User userEntity = userService.회원정보(id);
-        // model.addAttribute("user", userEntity);
+    public String userInfo(@PathVariable Integer id, Model model) {
+        User userEntity = userService.회원정보(id);
+        model.addAttribute("user", userEntity);
         return "user/updateForm";
     }
 
-    // 로그아웃 - 로그인O
     // @GetMapping("/logout")
     // public String logout() {
-    // session.invalidate(); //세션 무효화
+    // session.invalidate(); // 세션 무효화 (세션 아이디 영역의 데이터를 다 삭제해)
     // return "redirect:/";
     // }
 
-    // 웹브라우저 -> 회원가입 페이지 주세요 가능!
-    // 앱-> 회원가입 페이지 주세요 말이 안됨!!!
+    // 웹브라우저 -> 회원가입 페이지 주세요!!
+    // 앱 -> 회원가입 페이지 주세요!! 말이 안됨!!
     @GetMapping("/joinForm")
     public String joinForm() {
         return "user/joinForm";
@@ -43,11 +41,6 @@ public class UserController {
 
     @GetMapping("/loginForm")
     public String loginForm() {
-
-        // // 쿠키로직
-        // String cookieValue = request.getHeader("Cookie");
-
         return "user/loginForm";
     }
-
 }
